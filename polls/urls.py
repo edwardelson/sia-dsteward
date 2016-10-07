76bce283c8333943bf18451f4ad2a4be1dab42fb
edwardelson <edward_elson@rocketmail.com>
2016-10-07 15:58:19 +0800

    first draft

--- polls/urls.py
+++ polls/urls.py
@@ -4,13 +4,17 @@ from . import views	# url for view.py
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /
+    url(r'^$', views.main, name='main'),
+    # ex: /
+    url(r'^user/$', views.index, name='index'),
+    # ex: /steward
+    url(r'^steward/$', views.steward, name='steward'),
+    # ex: //5/
+    url(r'^user/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /5/results/
+    url(r'^user/(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /5/vote/
+    url(r'^user/(?P<question_id>[0-9]+)/seat/$', views.seat, name='seat'),
 ]
 
